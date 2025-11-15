ade30da04261563bbaac867189f3baa11f60bedf
aria_bz <ariabahmanzangi@gmail.com>
2025-11-15 22:36:45 +0330

    v3

--- src/Components/LandOfSite.tsx
+++ src/Components/LandOfSite.tsx
@@ -3,36 +3,37 @@ import OnePerson from "../img/onePerson.jpg";
 
 const LandOfSite = () => {
   return (
-    <section className=" pt-30 flex flex-col items-center justify-center">
-      <div className="flex justify-center items-center">
-        <div className="flex">
+    <section className="relative pt-30 flex flex-col items-center justify-center gap-6">
+      <div className="absolute top-28 -z-10 left-1/4 size-120 bg-green-300 blur-[100px] opacity-30"></div>
+      <div className="flex justify-items-center items-center gap-2">
+        <div className="flex relative w-10 pb-8">
           <img
-            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-1"
+            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-1 absolute right-16"
             src={OnePerson}
             alt=""
           />
           <img
-            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-2 relative right-2"
+            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-2 absolute right-12"
             src={OnePerson}
             alt=""
           />
           <img
-            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-3 relative right-5"
+            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-3 absolute right-8"
             src={OnePerson}
             alt=""
           />
           <img
-            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-4 relative right-8"
+            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-4 absolute right-4"
             src={OnePerson}
             alt=""
           />
           <img
-            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-5 relative right-11"
+            className="rounded-full size-8 object-cover border-2 border-white hover:-translate-y-0.5 transition z-5 absolute right-0"
             src={OnePerson}
             alt=""
           />
         </div>
-        <div>
+        <div className="flex flex-col ">
           <div className="flex">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -113,7 +114,30 @@ const LandOfSite = () => {
           <span>Used by 10,000+ users</span>
         </div>
       </div>
-      <div></div>
+      <div className="flex flex-col gap-8">
+        <span className="text-center text-6xl font-semibold">
+          Land your dream job with <br />
+          <span className="bg-linear-to-r from-green-700 to-green-600 bg-clip-text text-transparent">
+            AI-powered
+          </span>
+          resumes.
+        </span>
+        <span className="text-center text-lg">
+          Create, edit and download professional resumes with <br /> AI-powered
+          assistance.
+        </span>
+        <div className="flex gap-4 justify-center">
+          <button className="bg-green-500 hover:bg-green-600 text-white rounded-full px-9 h-12 ring-offset-2 ring-1 ring-green-400 flex items-center transition-colors cursor-pointer">
+            Get started
+          </button>
+          <button className="flex items-center gap-2 border border-slate-400 hover:bg-green-50 transition rounded-full px-7 h-12 text-slate-700 cursor-pointer">
+            Try demo
+          </button>
+        </div>
+      </div>
+      <span className="pt-10 text-slate-600">
+        Trusting by leading brands, including
+      </span>
     </section>
   );
 };
